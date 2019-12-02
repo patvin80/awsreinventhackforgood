c6bbe07e1cdb1c601b2869d486c7ee1ab027e6b5
Joe Hillhouse <joseph.hillhouse@ablevets.com>
2019-12-02 13:35:10 -0800

    Added search method

--- frontend/src/views/Counselor.vue
+++ frontend/src/views/Counselor.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="md-layout-item">
     <h1>This is the top page for counselors</h1>
-    <md-field>
-      <label>Fill out plan code if already have one</label>
-      <md-input v-model="planId" placeholder="plan number"></md-input>
-    </md-field>
+    <form novalidate class="md-layout" @submit="getPlan">
+      <md-field>
+        <label>Fill out plan code if already have one</label>
+        <md-input v-model="planId" placeholder="plan number"></md-input>
+        <md-button :md-ripple="false">Search</md-button>
+      </md-field>
+    </form>
     <md-button class="md-raised">
       <router-link to="/plan">Create or modify plan</router-link>
     </md-button>
@@ -12,13 +15,36 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
-      planId: null,
+      planId: null
+    };
+  },
+
+  methods: {
+    getPlan() {
+      console.log(this.planId);
+      axios.get('/')
+        .then(response => {
+          this.plan = response.data;
+          self.$router.push({
+            name: 'create or modify plan',
+            params: {
+                plan: {
+                    "a": "b"
+                }
+            }
+          })
+        })
+        .catch(e => {
+          console.log(e);
+        });
     }
   }
-}
+};
 </script>
 
 <style scoped>
