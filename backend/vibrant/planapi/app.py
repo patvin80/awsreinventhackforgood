bce77485e8126eab2766919bb374cbdcfaf81e55
Vinit Patankar <vpatankar@aarp.org>
2019-12-02 16:41:46 -0800

    backend checking

--- backend/vibrant/planapi/app.py
+++ backend/vibrant/planapi/app.py
@@ -1,5 +1,5 @@
 import json
-import os
+import boto3
 def lambda_handler(event, context):
     """Sample pure Lambda function
 
@@ -73,3 +73,61 @@ def lambda_handler(event, context):
             {"message": "hello world"}
         ),
     }
+
+def planquestions(event, context):
+    if (event["queryStringParameters"]["optin"] != "false"): 
+        with open('./models/planmock.json') as json_file:
+                data = json.load(json_file)
+    else:
+        with open('./models/planmocknoptin.json') as json_file:
+                data = json.load(json_file)
+    return {
+
+        "statusCode": 200,
+        "body": json.dumps(data),
+    }
+
+def planquestionexecution(event, context):
+    if (event["queryStringParameters"]["optin"] != "false"): 
+        with open('./models/planmockexecution.json') as json_file:
+                data = json.load(json_file)
+    else:
+        with open('./models/planmockexecutionnoptin.json') as json_file:
+            data = json.load(json_file)
+    return {
+
+        "statusCode": 200,
+        "body": json.dumps(data),
+    }
+
+def plancreationsave(event, context):
+    successmessage = "Success: Your Plan ID has been created - 1234"
+    body = event['body']
+    body_dict = json.loads(body)
+    if (body_dict['optin'] == "true"):
+        sns = boto3.client('sns')
+        number = '+14698671583'
+        sns.publish(PhoneNumber = number, Message='Plan ID has been created for you. Access it here with a Plan ID: 1234' )
+
+    return {
+
+        "statusCode": 200,
+        "body": successmessage,
+    }    
+
+def planoutboundcall(event, context):
+    connectclient = boto3.client('connect')
+
+    response = connectclient.start_outbound_voice_contact(
+        ContactFlowId= "a4996f21-169e-4ede-b295-af76a85ad720",
+        DestinationPhoneNumber="+14698671583",
+        InstanceId="17bbff90-8b60-4a39-92f3-fd6153203842",
+        QueueId= "f8695c8f-2721-489c-b10b-73f0fc980775",
+        SourcePhoneNumber="+18335188455"
+    )
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {"message": "hello world"}
+        )
+    } 
