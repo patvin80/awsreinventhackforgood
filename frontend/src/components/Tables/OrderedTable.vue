a94cf2bcbc919737c9cc5fdcbce1633266410de0
Joe Hillhouse <joseph.hillhouse@ablevets.com>
2019-12-02 17:24:44 -0800

    Added plan list wth risk score

--- frontend/src/components/Tables/OrderedTable.vue
+++ frontend/src/components/Tables/OrderedTable.vue
@@ -3,10 +3,15 @@
     <md-table v-model="users" :table-header-color="tableHeaderColor">
       <md-table-row slot="md-table-row" slot-scope="{ item }">
         <md-table-cell md-label="ID">{{ item.id }}</md-table-cell>
-        <md-table-cell md-label="Name">{{ item.name }}</md-table-cell>
-        <md-table-cell md-label="Salary">{{ item.salary }}</md-table-cell>
-        <md-table-cell md-label="Country">{{ item.country }}</md-table-cell>
-        <md-table-cell md-label="City">{{ item.city }}</md-table-cell>
+        <md-table-cell md-label="Opt-in Status">{{ item.status }}</md-table-cell>
+        <md-table-cell md-label="Risk Score">{{ item.score }}</md-table-cell>
+        <md-table-cell md-label>
+          <md-button
+            class="md-raised md-success"
+            :disabled="item.status != 'Yes' || item.score < 7"
+            @click="call()"
+          >Follow-up Call</md-button>
+        </md-table-cell>
       </md-table-row>
     </md-table>
   </div>
@@ -26,35 +31,47 @@ export default {
       selected: [],
       users: [
         {
-          id: 1,
-          name: "Dakota Rice",
-          salary: "$36,738",
-          country: "Niger",
-          city: "Oud-Turnhout"
+          id: 12345,
+          status: "Yes",
+          score: 10
         },
         {
-          id: 2,
-          name: "Minerva Hooper",
-          salary: "$23,738",
-          country: "Curaçao",
-          city: "Sinaai-Waas"
+          id: 12346,
+          status: "No",
+          score: 8
         },
         {
-          id: 3,
-          name: "Sage Rodriguez",
-          salary: "$56,142",
-          country: "Netherlands",
-          city: "Overland Park"
+          id: 12347,
+          status: "Yes",
+          score: 2
         },
         {
-          id: 4,
-          name: "Philip Chaney",
-          salary: "$38,735",
-          country: "Korea, South",
-          city: "Gloucester"
+          id: 12348,
+          status: "No",
+          score: 1
         }
       ]
     };
+  },
+  methods: {
+    call() {
+      this.notifyVue();
+
+      const url =
+        "https://d0dcpgvcla.execute-api.us-east-1.amazonaws.com/Prod/planoutboundcall";
+      fetch(url).then(res => {});
+    },
+    notifyVue() {
+      var color = Math.floor(Math.random() * 4 + 1);
+      this.$notify({
+        message:
+          "Success - Your follow-up has been initiated.",
+        icon: "add_alert",
+        horizontalAlign: 'bottom',
+        verticalAlign: 'right',
+        type: 'success'
+      });
+    }
   }
 };
 </script>
--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -13,8 +13,21 @@
               </div>
             </div>
             <div class="md-layout-item md-size-100 text-right">
+              <md-checkbox v-model="optIn" :disabled="this.planID != ''">Opt-in for notifications</md-checkbox>
               <md-button class="md-raised md-success" @click="createOrFetchPlan">{{ buttonText }}</md-button>
             </div>
+          </md-card-content>
+        </md-card>
+        <md-card>
+          <md-card-header data-background-color="orange">
+            <h4 class="title">Safety Plans</h4>
+            <p
+              class="category"
+              style="font-weight:bold; color:#000"
+            >View list of safety plans that need follow up</p>
+          </md-card-header>
+          <md-card-content>
+            <ordered-table table-header-color="orange"></ordered-table>
           </md-card-content>
         </md-card>
       </div>
@@ -31,7 +44,6 @@ import {
   OrderedTable
 } from "@/components";
 
-
 export default {
   components: {
     StatsCard,
@@ -42,40 +54,49 @@ export default {
   },
   data() {
     return {
-      planID: '',
+      planID: "",
+      optIn: false
     };
   },
   computed: {
     buttonText() {
-      return this.planID === '' ? 'CREATE PLAN' : 'MANAGE PLAN'
+      return this.planID === "" ? "CREATE PLAN" : "MANAGE PLAN";
     },
     hasPlanIDSet() {
-      return !!window.planID
+      return !!window.planID;
     }
   },
   methods: {
     createOrFetchPlan() {
-      if (this.planID === '') {
-        const planID = this.generatePlanID()
-        console.log({ planID })
-        this.$store.commit('updatePlanID', planID)
-        this.$router.push({ path: 'warning-signs' })
+      if (this.planID === "") {
+        this.generatePlanID();
       }
 
-      const url = 'https://d0dcpgvcla.execute-api.us-east-1.amazonaws.com/Prod/plangetapi?optin=false'
+      const url =
+        "https://d0dcpgvcla.execute-api.us-east-1.amazonaws.com/Prod/plangetapi?optin=false";
       fetch(url)
         .then(res => {
-          return res.text()
+          return res.text();
         })
         .then(json => {
-          console.log({ json: JSON.parse(json) })
-          this.$store.commit('updatePlanID', this.planID)
-          this.$store.commit('updatePlanData', JSON.parse(json))
-          this.$router.push({ path: 'warning-signs' })
-        })
+          console.log({ json: JSON.parse(json) });
+          this.$store.commit("updatePlanID", this.planID);
+          this.$store.commit("updatePlanData", JSON.parse(json));
+          this.$router.push({ path: "warning-signs" });
+        });
     },
     generatePlanID() {
-      return Math.random().toString().slice(2,7);
+      if (this.optIn) {
+        const url =
+          "https://d0dcpgvcla.execute-api.us-east-1.amazonaws.com/Prod/plansave";
+
+        fetch(url, { method: "post", body: '{ "optin": "true" }' }).then(
+          res => {
+            this.$store.commit("updatePlanID", 1234);
+            this.$router.push({ path: "warning-signs" });
+          }
+        );
+      }
     }
   }
 };
